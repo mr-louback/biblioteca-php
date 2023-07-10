37595c63aaec91310bdf34361b85ae4fbac1126a
mr-louback <loubacked4work@outlook.com>
2023-07-10 12:23:23 -0300

    remocao de pasta methods

--- methods/pessoa.php
+++
@@ -1,5 +0,0 @@
-<?php
-// I would like to implements a class who's responsable to 
-// read the database and catch those's informations as atribuits of 
-// the class and use to implements an a session.
-// var_dump($GLOBALS);
